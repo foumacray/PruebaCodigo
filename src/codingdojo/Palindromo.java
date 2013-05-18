7f36a4a4bc5806c612c38378437871c19cf58c6a
usuario <usuario@10.22.146.42>
2013-05-18 10:31:31 +0100

    SomeChanges...

--- src/codingdojo/Palindromo.java
+++ src/codingdojo/Palindromo.java
@@ -2,13 +2,18 @@ package codingdojo;
 
 public class Palindromo {
 
-    int number;
+    String numberOnString;
 
     public Palindromo(int i) {
-        number = i;
+        numberOnString = String.valueOf(i);
     }
 
     public boolean isPalindromo() {
-        return false;
+        for(int indice=0;indice<numberOnString.length();indice++){
+            if (!(numberOnString.substring(indice, indice).equals(numberOnString.substring(numberOnString.length()-indice, numberOnString.length()-indice)))){
+                return false;
+            }
+        }
+        return true;
     }
 }

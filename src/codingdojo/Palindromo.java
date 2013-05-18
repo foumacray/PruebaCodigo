33efe7b0c3c44566b596c72ab561685013d865df
usuario <usuario@10.22.146.42>
2013-05-18 10:36:44 +0100

    Some Changes more...

--- src/codingdojo/Palindromo.java
+++ src/codingdojo/Palindromo.java
@@ -10,7 +10,9 @@ public class Palindromo {
 
     public boolean isPalindromo() {
         for(int indice=0;indice<numberOnString.length();indice++){
-            if (!(numberOnString.substring(indice, indice).equals(numberOnString.substring(numberOnString.length()-indice, numberOnString.length()-indice)))){
+            String front=numberOnString.substring(indice, indice+1);
+            String back=numberOnString.substring(numberOnString.length()-indice-1, numberOnString.length()-indice);
+            if (!front.equals(back)){
                 return false;
             }
         }
